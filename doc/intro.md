59e74963bec481748739dc9256b6095f0e529766
Alexander Pashkov <alexpashkov123@gmail.com>
2018-11-17 15:29:35 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to npclj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/npclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns npclj.core-test
+  (:require [clojure.test :refer :all]
+            [npclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
