a68120524e6a106d2028e0ff8da6e81470d16391
Alexander Pashkov <alexpashkov123@gmail.com>
2018-11-17 15:55:43 +0200

    add simple read

--- src/npclj/core.clj
+++ src/npclj/core.clj
@@ -1,7 +1,17 @@
 (ns npclj.core
   (:gen-class))
 
+(str "asdffsad" "asdf")
+
+(->> "asdf"
+     (map identity)
+     (apply str))
+
+(defn read-pzl [from]
+  (doseq [line (line-seq (java.io.BufferedReader. from))]
+    (println line)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (read-pzl *in*))
