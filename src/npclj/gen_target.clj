26cdd4d43b5792b074cf9dfab28031468e466d5e
Alexander Pashkov <alexpashkov123@gmail.com>
2018-11-18 15:32:29 +0200

    create test file

--- src/npclj/gen_target.clj
+++ src/npclj/gen_target.clj
@@ -4,9 +4,6 @@
 (defn- get-tile [pzl coords]
   (get-in pzl (reverse coords)))
 
-(defn- inside? [pzl coords]
-  (number? (get-tile pzl coords)))
-
 (defn- next-coords [coords dir]
   (map + coords dir))
 
@@ -32,19 +29,3 @@
                (inc i)
                (next-coords coords dir)
                dir)))))
-
-(= (gen-target 1) [[0]])
-(= (gen-target 2) [[1 2]
-                   [0 3]])
-(= (gen-target 3) [[1 2 3]
-                   [8 0 4]
-                   [7 6 5]])
-(= (gen-target 4) [[1 2 3 4]
-                   [12 13 14 5]
-                   [11 0 15 6]
-                   [10 9 8 7]])
-(= (gen-target 5) [[1 2 3 4 5]
-                   [16 17 18 19 6]
-                   [15 24 0 20 7]
-                   [14 23 22 21 8]
-                   [13 12 11 10 9]])
---
+++ test/npclj/gen_target_test.clj
@@ -0,0 +1,21 @@
+(ns npclj.gen-target-test
+  (:require [clojure.test :refer :all]
+            [npclj.gen-target :refer :all]))
+
+(deftest gen-target-test
+  (testing "gen-target"
+    (is (= (gen-target 1) [[0]]))
+    (is (= (gen-target 2) [[1 2]
+                           [0 3]]))
+    (is (= (gen-target 3) [[1 2 3]
+                           [8 0 4]
+                           [7 6 5]]))
+    (is (= (gen-target 4) [[1 2 3 4]
+                           [12 13 14 5]
+                           [11 0 15 6]
+                           [10 9 8 7]]))
+    (is (= (gen-target 5) [[1 2 3 4 5]
+                           [16 17 18 19 6]
+                           [15 24 0 20 7]
+                           [14 23 22 21 8]
+                           [13 12 11 10 9]]))))
