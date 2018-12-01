a7221dad696e7b0af875ca9eb361e8ec94b2db2c
Alexander Pashkov <alexpashkov123@gmail.com>
2018-12-01 17:36:17 +0200

    Memoize find-tile

--- src/npclj/puzzle.clj
+++ src/npclj/puzzle.clj
@@ -3,14 +3,16 @@
 
 (def directions [[1 0] [0 1] [-1 0] [0 -1]])
 
-(defn find-tile [pzl tile]
-  "Find coordinates of a tile, returns nil if not found"
-  (loop [y 0]
-    (when (< y (count pzl))
-      (let [x (.indexOf (pzl y) tile)]
-        (if (>= x 0)
-          [x y]
-          (recur (inc y)))))))
+(def find-tile
+  (memoize
+    (fn [pzl tile]
+      "Find coordinates of a tile, returns nil if not found"
+      (loop [y 0]
+        (when (< y (count pzl))
+          (let [x (.indexOf (pzl y) tile)]
+            (if (>= x 0)
+              [x y]
+              (recur (inc y)))))))))
 
 (defn get-tile [pzl coords]
   (get-in pzl (reverse coords)))
