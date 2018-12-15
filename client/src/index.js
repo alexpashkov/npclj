a137ee949e7621847186025f77fb605dce1252f7
Alexander Pashkov <alexpashkov123@gmail.com>
2018-12-15 20:33:27 +0200

    Add animation.

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ const getJSON = (url, options) => fetch(url, options).then(res => res.json());
 
 class App extends Component {
   state = {
+    solving: false,
     puzzle: [[1, 2, 3], [8, 0, 4], [7, 6, 5]],
     size: 4
   };
@@ -21,19 +22,37 @@ class App extends Component {
     this.setState({ size: +value });
 
   solve = () => {
+    this.setState({ solving: true });
     getJSON(ROOT_URL, {
       method: "POST",
       body: JSON.stringify({ puzzle: this.state.puzzle })
-    }).then(data => {
-      debugger;
-    });
+    })
+      .then(({ status, result }) => {
+        if (status !== "ok") throw new Error(status);
+        const showStates = states => {
+          if (!states || !states.length) {
+            return this.setState({ solving: false });
+          }
+          this.setState({
+            puzzle: states[0]
+          });
+          setTimeout(showStates, 100, states.slice(1));
+        };
+        showStates(result.states);
+      })
+      .catch(err => {
+        console.warn(err);
+        this.setState({ solving: false });
+      });
   };
 
   render() {
-    const { puzzle, size } = this.state;
+    const { puzzle, size, solving } = this.state;
     return (
       <div>
-        <button onClick={this.generate}>Generate</button>
+        <button onClick={this.generate} disabled={solving}>
+          Generate
+        </button>
         <input
           type="number"
           min="3"
@@ -41,7 +60,9 @@ class App extends Component {
           onChange={this.onSizeChange}
         />
         <Puzzle>{puzzle}</Puzzle>
-        <button onClick={this.solve}>Solve!</button>
+        <button onClick={this.solve} disabled={solving}>
+          Solve!
+        </button>
       </div>
     );
   }
