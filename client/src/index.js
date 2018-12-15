57ed6249d569e49e0c3b21022f0534e7e50ffd25
Alexander Pashkov <alexpashkov123@gmail.com>
2018-12-15 18:18:35 +0200

    Add basic index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,50 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import Puzzle from "./Puzzle";
 
-
 class App extends Component {
-    state = {
-        pzl: [
-            [1, 2, 3],
-            [8, 0, 4],
-            [7, 6, 5],
-        ],
-        size: 4
-    };
+  state = {
+    puzzle: [[1, 2, 3], [8, 0, 4], [7, 6, 5]],
+    size: 4
+  };
 
-    onSizeChange = ({currentTarget: {value}}) => this.setState({size: value});
-    solve = () => {
-        fetch('http://localhost:3000', {
-            method: 'POST',
-            body: JSON.stringify({puzzle: this.state.pzl})
-        }).then(d => d.text()).then(console.log)
-    }
+  generate = () => {
+    this.setState({
+      puzzle: generate(this.state.size)
+    });
+  };
 
-    render() {
-        const {pzl, size} = this.state;
-        return (
-            <div>
-                <button>Generate</button>
-                <input type="number" min="3" value={size} onChange={this.onSizeChange}/>
-                <Puzzle>{pzl}</Puzzle>
-                <button onClick={this.solve}>Solve!</button>
-            </div>
-        );
-    }
+  onSizeChange = ({ currentTarget: { value } }) =>
+    this.setState({ size: +value });
+
+  solve = () => {
+    fetch("http://localhost:3000", {
+      method: "POST",
+      body: JSON.stringify({ puzzle: this.state.puzzle })
+    })
+      .then(d => d.text())
+      .then(data => {
+        debugger;
+      });
+  };
+
+  render() {
+    const { puzzle, size } = this.state;
+    return (
+      <div>
+        <button onClick={this.generate}>Generate</button>
+        <input
+          type="number"
+          min="3"
+          value={size}
+          onChange={this.onSizeChange}
+        />
+        <Puzzle>{puzzle}</Puzzle>
+        <button onClick={this.solve}>Solve!</button>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
