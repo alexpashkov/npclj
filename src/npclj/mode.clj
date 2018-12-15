a06610ca847f8115ff67d39cef51e14dcc5cb5cd
Eric Lyahovets <elyahove@e3r2p7.unit.ua>
2018-12-15 16:08:20 +0200

    Improve HTTP mode.
    	Add static files route.
    	Add server logging.
    	Refactor.

--- src/npclj/mode.clj
+++ src/npclj/mode.clj
@@ -1,15 +1,9 @@
 (ns npclj.mode
-  (:require [org.httpkit.server :as http-kit]
-            [clojure.string :as str]
-            [clojure.java.io :as io]
-            [npclj.parser :refer [parse]]
+  (:require [npclj.parser :refer [parse]]
             [npclj.solve :refer [solve]]
             [npclj.puzzle :as puzzle]
-            [npclj.heuristic :as heuristic]
             [npclj.is-solvable :refer [solvable?]]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [clojure.data.json :as json])
+            [npclj.http :refer [http-mode]])
   (:gen-class))
 
 (defn cli-mode [heuristic-fn]
@@ -31,44 +25,6 @@
           (println "The puzzle isn't solvable")))
       (println "Failed to read a puzzle"))))
 
-(defn homepage-handler [heur]
-  (fn [req]
-      {:status  200
-       :headers {"Content-Type" "text/html"}
-       :body    "Homepage."}))
-
-(defn solve-to-json
-  [body heuristic-fn]
-  (json/write-str
-    (try
-      (if-let
-        [json-req (json/read (io/reader body) :encoding "UTF-8")]
-        (do
-          (prn json-req)
-          (if-let
-            [pzl (json-req "puzzle")]
-            (if (puzzle/valid? pzl)
-              (if (solvable? pzl)
-                { :status :ok :solved (solve pzl heuristic-fn) }
-                { :status :error :error "Puzzle is not solvable" })
-              { :status :error :error "Invalid puzzle." })
-            { :status :error :error "No puzzle provided." }))
-          { :status :error :error "Failed to read JSON." })
-      (catch Exception e { :status :error :error (str e) }))))
-
-(defn solver-handler [heur]
-  (fn [req]
-    {:status  200
-     :headers {"Content-Type" "text/html"}
-     :body    (solve-to-json (:body req) heur)}))
-
-(defn server-mode [heuristic-fn]
-  (do
-    (defroutes np-routes
-      (GET "/" [] (homepage-handler heuristic-fn))
-      (POST "/" [] (solver-handler heuristic-fn)))
-    (http-kit/run-server #'np-routes {:port 3000})))
-
 (def np-modes
   { "cli"    cli-mode
-    "http"   server-mode})
+    "http"   http-mode})
